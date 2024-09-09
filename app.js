3ad8de5d24741ccc0bfe9f609ebb543c179c1c89
sergiopon <1105366347@javerianacali.edu.co>
2024-09-09 06:45:17 -0500

    Se actualiza lo hecho por isabella

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import ParcheCard from './ParcheCardV1';
+
+
+function App() {
+    return (
+        <div>
+            <ParcheCard />
+        </div>
+    );
+}
+
+export default App;
